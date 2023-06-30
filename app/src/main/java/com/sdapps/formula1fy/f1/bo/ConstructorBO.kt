d04ffa12a86ed08d46182f9ba30db750a1c77f4b
dhanush.s <dhanush.s@ivymobility.com>
2023-06-30 16:11:53 +0530

    ui change

--- app/src/main/java/com/sdapps/formula1fy/f1/bo/ConstructorBO.kt
+++ app/src/main/java/com/sdapps/formula1fy/f1/bo/ConstructorBO.kt
@@ -1,6 +1,8 @@
 package com.sdapps.formula1fy.f1.bo
 
 class ConstructorBO {
+    var driver1 :String? = null
+    var driver2: String? = null
     var position: String? = null
     var points: String? = null
     var wins: String? = null
